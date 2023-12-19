62b0e703d579b528fa9a6b82a032719c3df24029
LunaTheFoxgirl <cli-luna@protonmail.com>
2023-12-19 20:01:19 +0100

    add minimal_rt mode

--- source/numem/package.d
+++ source/numem/package.d
@@ -2,8 +2,8 @@ module numem;
 import std.conv : emplace;
 import core.stdc.stdlib : free, exit, malloc;
 import std.traits;
+import core.stdc.stdio : printf;
 
-extern(C):
 nothrow @nogc:
 
 //
@@ -32,7 +32,60 @@ private {
         else
             static assert(false);
     }
+
+    version(minimal_rt) {
+        
+        struct classDestructorList(T) {
+        @nogc nothrow:
+            void function(T) destructorFunc;
+
+            void initialize() {
+                destructorFunc = (T instance) {
+                    import std.traits : BaseTypeTuple;
+
+                    // Scope for base class destructor
+                    static foreach(item; BaseClassesTuple!T) {
+                        {
+                            static if (!is(item == Object) && !is(item == T)) {
+                                static if (__traits(hasMember, item, "__xdtor")) {
+                                    auto dtorptr = &item.init.__xdtor;
+                                    dtorptr.ptr = cast(void*)instance;
+                                    dtorptr();
+                                } else static if (__traits(hasMember, item, "__dtor")) {
+                                    auto dtorptr = &item.init.__dtor;
+                                    dtorptr.ptr = cast(void*)instance;
+                                    dtorptr();
+                                }
+                            }
+                        }
+                    }
+
+                    // Scope for self destructor
+                    {
+                        static if (__traits(hasMember, T, "__xdtor")) {
+                            auto dtorptr = &T.init.__xdtor;
+                            dtorptr.ptr = cast(void*)instance;
+                            dtorptr();
+                        } else static if (__traits(hasMember, T, "__dtor")) {
+                            auto dtorptr = &T.init.__dtor;
+                            dtorptr.ptr = cast(void*)instance;
+                            dtorptr();
+                        }
+                    }
+                };
+            }
+
+            /**
+                Destruct
+            */
+            void destruct(T instance) {
+                destructorFunc(instance);
+            }
+        }
+    }
 }
+
+extern(C):
 
 /**
     Allocates a new struct on the heap.
@@ -55,14 +108,34 @@ T* nogc_new(T, Args...)(Args args) if (is(T == struct)) {
     Immediately exits the application if out of memory.
 */
 T nogc_new(T, Args...)(Args args) if (is(T == class)) {
-    immutable size_t allocSize = __traits(classInstanceSize, T);
-    void* rawMemory = malloc(allocSize);
-    if (!rawMemory) {
-        exit(-1);
-    }
+    version(minimal_rt) {
+        immutable(size_t) destructorObjSize = classDestructorList!T.sizeof;
+        immutable(size_t) classObjSize = __traits(classInstanceSize, T);
+        immutable size_t allocSize = classObjSize + destructorObjSize;
 
-    T obj = emplace!T(rawMemory[0 .. allocSize], args);
-    return obj;
+        void* rawMemory = malloc(allocSize);
+        if (!rawMemory) {
+            exit(-1);
+        }
+
+        // Allocate class destructor list
+        auto dlist = emplace!(classDestructorList!(T))(rawMemory[0..destructorObjSize]);
+        dlist.initialize();
+
+        // Allocate class
+        T obj = emplace!T(rawMemory[destructorObjSize .. allocSize], args);
+        return obj;
+
+    } else {
+        immutable size_t allocSize = __traits(classInstanceSize, T);
+        void* rawMemory = malloc(allocSize);
+        if (!rawMemory) {
+            exit(-1);
+        }
+
+        T obj = emplace!T(rawMemory[0 .. allocSize], args);
+        return obj;
+    }
 }
 
 /**
@@ -71,28 +144,54 @@ T nogc_new(T, Args...)(Args args) if (is(T == class)) {
     For structs this will call the struct's destructor if it has any.
 */
 void nogc_delete(T)(ref T obj_)  {
-    static if (isPointer!T || is(T == class)) {
-        if (obj_) {
-        
-            static if (is(T == class) || is(T == struct)) {
+    
+    version(minimal_rt) {
+        static if (isPointer!T || is(T == class)) {
+            if (obj_) {
+            
+                static if (is(T == class)) {
 
-                // Try to call elaborate destructor first before attempting __dtor
-                static if (__traits(hasMember, T, "__xdtor")) {
-                    assumeNothrowNoGC(&obj_.__xdtor)();
-                } else static if (__traits(hasMember, T, "__dtor")) {
-                    assumeNothrowNoGC(&obj_.__dtor)();
+                    // Do pointer arithmetic to fetch the class destructor list and call it.
+                    void* chunkStart = (cast(void*)obj_)-(classDestructorList!T).sizeof;
+                    classDestructorList!(T)* destructorList = (cast(classDestructorList!(T)*)chunkStart);
+                    destructorList.destruct(obj_);
+                    free(chunkStart);
+
+                } else static if (is(T == struct)) {
+
+                    // Try to call elaborate destructor first before attempting __dtor
+                    static if (__traits(hasMember, T, "__xdtor")) {
+                        assumeNothrowNoGC(&obj_.__xdtor)();
+                    } else static if (__traits(hasMember, T, "__dtor")) {
+                        assumeNothrowNoGC(&obj_.__dtor)();
+                    }
+                    free(cast(void*)obj_);
                 }
             }
+        } else static if (is(T == struct)) {
 
-            free(cast(void*)obj_);
+            // Try to call elaborate destructor first before attempting __dtor
+            static if (__traits(hasMember, T, "__xdtor")) {
+                assumeNothrowNoGC(&obj_.__xdtor)();
+            } else static if (__traits(hasMember, T, "__dtor")) {
+                assumeNothrowNoGC(&obj_.__dtor)();
+            }
         }
-    } else static if (is(T == struct)) {
 
-        // Try to call elaborate destructor first before attempting __dtor
-        static if (__traits(hasMember, T, "__xdtor")) {
-            assumeNothrowNoGC(&obj_.__xdtor)();
-        } else static if (__traits(hasMember, T, "__dtor")) {
-            assumeNothrowNoGC(&obj_.__dtor)();
+    } else {
+
+        // With a normal runtime we can use destroy
+        static if (isPointer!T || is(T == class)) {
+            static if (is(T == class)) {
+                auto objptr_ = obj_;
+            } else {
+                auto objptr_ = &obj_;
+            }
+
+            destroy!false(objptr_);
+        } else static if (is(T == struct)) {
+            auto objptr_ = &obj_;
+            destroy!false(objptr_);
         }
     }
 }
