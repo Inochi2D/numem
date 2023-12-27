45d0da7c985c0eda69ad786d6d8b5ae797676d96
LunaTheFoxgirl <cli-luna@protonmail.com>
2023-12-27 01:15:08 +0100

    Add numem.all module

---
+++ source/numem/all.d
@@ -0,0 +1,10 @@
+/**
+    Automatically imports all of the numem types.
+*/
+module numem.all;
+
+public import numem;
+public import numem.ptr;
+public import numem.string;
+public import numem.vector;
+
