1145491a8e8e777a6b3aca4040f839ff11b04529
LunaTheFoxgirl <cli-luna@protonmail.com>
2024-02-10 06:38:30 +0100

    Add exists function to file

--- source/numem/io/file.d
+++ source/numem/io/file.d
@@ -35,3 +35,36 @@ shared_ptr!FileStream openFileShared(nstring filename, scope const(char)* mode) 
 unique_ptr!FileStream openFileOwned(nstring filename, scope const(char)* mode) {
     return unique_new!FileStream(c.fopen(filename.toCString, mode));
 }
+
+/**
+    Checks whether a file exists at the specified path
+*/
+bool exists(nstring str) {
+    version(Posix) {
+        import uni = core.sys.posix.unistd;
+        return uni.access(str.toCString, F_OK) == 0;
+    } else version(Windows) {
+        import win = core.sys.windows.core;
+        
+        nwstring tstr;
+
+        // Find out how many characters there is to convert to UTF-16
+        size_t reqLength = win.MultiByteToWideChar(win.CP_UTF8, 0, str.toCString, -1, null, 0);
+        tstr.resize(reqLength);
+
+        // Convert to UTF-16
+        win.MultiByteToWideChar(win.CP_UTF8, 0, str.toCString, -1, cast(wchar*)tstr.toCString, cast(int)tstr.length);
+
+        bool exists = win.GetFileAttributesW(tstr.toCString) != win.INVALID_FILE_ATTRIBUTES;
+        
+        // Free memory from the temporary string
+        nogc_delete(tstr);
+        return exists;
+    } else {
+        auto f = c.fopen(str.toCString, "r");
+        if (f) {
+            c.fclose(f);
+        }
+        return f !is null;
+    }
+}
