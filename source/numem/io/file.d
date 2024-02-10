72328ad7e61229e6999d6fab104068630e5c35ca
LunaTheFoxgirl <cli-luna@protonmail.com>
2024-02-10 06:13:35 +0100

    Add openFileShared and openFileOwned

--- source/numem/io/file.d
+++ source/numem/io/file.d
@@ -7,13 +7,31 @@
 module numem.io.file;
 import numem.mem.string;
 import numem.mem;
+import numem.mem.ptr;
 import numem.stream.filestream;
 
 import c = core.stdc.stdio;
 
+@nogc nothrow:
+public:
+
 /**
     Opens a file stream from the specified file path
 */
-FileStream open(nstring filename, scope const(char)* mode) {
+FileStream openFile(nstring filename, scope const(char)* mode) {
     return nogc_new!FileStream(c.fopen(filename.toCString, mode));
+}
+
+/**
+    Opens a file stream from the specified file path as a shared pointer
+*/
+shared_ptr!FileStream openFileShared(nstring filename, scope const(char)* mode) {
+    return shared_new!FileStream(c.fopen(filename.toCString, mode));
+}
+
+/**
+    Opens a file stream from the specified file path as a unique pointer
+*/
+unique_ptr!FileStream openFileOwned(nstring filename, scope const(char)* mode) {
+    return unique_new!FileStream(c.fopen(filename.toCString, mode));
 }
