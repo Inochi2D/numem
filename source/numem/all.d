58e8df5e39e8e3304371932fa9d795742abcc523
LunaTheFoxgirl <cli-luna@protonmail.com>
2024-04-09 02:44:19 +0200

    Import map and set from all

--- source/numem/all.d
+++ source/numem/all.d
@@ -7,4 +7,6 @@ public import numem.mem;
 public import numem.mem.ptr;
 public import numem.mem.string;
 public import numem.mem.vector;
+public import numem.mem.map;
+public import numem.mem.set;
 public import numem.stream;
