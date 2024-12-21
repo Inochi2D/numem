bd76c96476de5758c45c24491d913b2bb6e85bf8
LunaTheFoxgirl <cli-luna@protonmail.com>
2024-12-21 19:51:10 +0100

    Add math module

--- source/numem/math.d
+++ source/numem/math.d
@@ -11,3 +11,26 @@
 module numem.math;
 
 public import numem.core.bitop;
+
+nothrow @nogc:
+
+/**
+    Returns the larger of the 2 given scalar values.
+*/
+T max(T)(T lhs, T rhs) if (__traits(isScalar, T)) {
+    return lhs > rhs ? lhs : rhs;
+}
+
+/**
+    Returns the smaller of the 2 given scalar values.
+*/
+T min(T)(T lhs, T rhs) if (__traits(isScalar, T)) {
+    return lhs < rhs ? lhs : rhs;
+}
+
+/**
+    Clamps scalar value into the given range.
+*/
+T clamp(T)(T value, T min_, T max_) if (__traits(isScalar, T))  {
+    return min(max(value, min_), max_);
+}
