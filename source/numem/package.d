dd98e8e78f9f94f93806106dede6d0f435f73d05
LunaTheFoxgirl <cli-luna@protonmail.com>
2025-02-14 23:20:06 +0100

    Add nu_get_version

--- source/numem/package.d
+++ source/numem/package.d
@@ -14,3 +14,33 @@ public import numem.core.exception;
 public import numem.casting;
 public import numem.lifetime;
 public import numem.object;
+
+/**
+    Container for numem version.
+*/
+struct nu_version {
+@nogc:
+public:
+    uint major;
+    uint minor;
+    uint patch;
+}
+
+/**
+    Numem version
+*/
+private
+const __gshared nu_version __nu_version = nu_version(1, 0, 0);
+
+/**
+    Gets the current version of numem.
+
+    Returns:
+        The current version of numem as 3 tightly packed 32-bit
+        unsigned integers.
+*/
+export
+extern(C)
+nu_version nu_get_version() @nogc nothrow @safe pure {
+    return __nu_version;
+}
