057f10e8ec9d17ca8f42e10f805c65859727584d
LunaTheFoxgirl <luna@foxgirls.gay>
2025-05-11 02:40:15 +0200

    Make nogc_move and nogc_copy clamp to smallest range.

--- source/numem/lifetime.d
+++ source/numem/lifetime.d
@@ -400,7 +400,10 @@ void nogc_emplace(T, Args...)(auto ref T dest, Args args) @trusted {
     reset to their base initialized state before any constructors are run.
 */
 void nogc_move(T)(T[] dst, T[] src) @trusted {
-    foreach(i; 0..src.length)
+    import nulib.math : min;
+    size_t toTx = min(dst.length, src.length);
+
+    foreach(i; 0..toTx)
         __move(src[i], dst[i]);
 }
 
@@ -410,7 +413,10 @@ void nogc_move(T)(T[] dst, T[] src) @trusted {
     Postblits and copy constructors will be called subsequently.
 */
 void nogc_copy(T)(T[] dst, T[] src) @trusted {
-    foreach(i; 0..src.length)
+    import nulib.math : min;
+    size_t toTx = min(dst.length, src.length);
+
+    foreach(i; 0..toTx)
         __copy(src[i], dst[i]);
 }
 
