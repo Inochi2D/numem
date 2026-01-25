f4e42bff80e47401aac59186414d5614b170c20b
LunaTheFoxgirl <luna@foxgirls.gay>
2026-01-25 16:41:33 +0100

    Clean up numem.rc

--- source/numem/rc.d
+++ source/numem/rc.d
@@ -9,25 +9,78 @@
     Authors:    Luna Nielsen
 */
 module numem.rc;
+import numem.core.lifetime;
 import numem.core.hooks;
+import numem.core.traits;
 import numem.lifetime;
 
 /**
-    A reference counted container.
+    A semi-automatically reference counted value container.
 */
 struct Rc(T) {
-public:
+private:
 @nogc:
-    mixin RcBase!T;
+    alias RCT = __rc!T;
+    RCT* rc;
+
+    pragma(inline, true)
+    void __retain() @trusted {
+        if (rc) {
+            nu_atomic_add_32(rc.refs, 1);
+        }
+    }
+
+    pragma(inline, true)
+    void __release() @trusted {
+        if (rc) {
+            if (nu_atomic_sub_32(rc.refs, 1) == 1) {
+                nogc_trydelete(rc.value);
+                nu_free(rc);
+
+            }
+        }
+        this.rc = null;
+    }
+
+public:
+    alias value this;
 
     /**
-        Creates an reference counted object using given $(D value).
+        Whether the RC value is valid.
+    */
+    @property bool isValid() inout nothrow @safe => rc !is null;
+
+    /**
+        The value stored in the rc type.
+    */
+    @property ref inout(T) value() inout nothrow @safe => cast(inout(T))rc.value;
+
+    /// Destructor
+    ~this() {
+        this.__release();
+    }
+
+    /**
+        Creates an reference counted object using given $(D value),
+        if value is given by reference, it is moved to the Rc.
 
         Params:
             value = The value.
     */
-    this(T value) @trusted {
-        this.rc = nogc_new!__rc(1, value);
+    this(in inout(T) value) @trusted {
+        RCT* rc = cast(RCT*)nu_malloc(RCT.sizeof);
+        rc.value = cast(T)value;
+        rc.refs = 1;
+
+        nu_atomic_store_ptr(cast(void**)&this.rc, rc);
+    }
+
+    /**
+        Copy constructor
+    */
+    this(ref return scope inout(typeof(this)) rhs) @trusted {
+        this.rc = cast(RCT*)rhs.rc;
+        this.__retain();
     }
 
     /**
@@ -46,76 +99,28 @@ public:
         this.__release();
         return this;
     }
-}
-
-/**
-    Automatically reference counted value.
-*/
-struct Arc(T) {
-public:
-@nogc:
-    mixin RcBase!T;
-
-    /// Destructor
-    ~this() {
-        this.__release();
-    }
 
     /**
-        Creates an reference counted object using given $(D value).
-
-        Params:
-            value = The value.
+        Implements implicit cast for RC value validity.
     */
-    this(T value) @trusted {
-        this.rc = nogc_new!__rc(1, value);
-    }
+    T opCast(T : bool)() inout nothrow @safe => isValid();
 
     /**
-        Copy constructor
+        Implements base cast
     */
-    this(ref return scope inout(typeof(this)) rhs) inout @trusted {
-        this.rc = rhs.rc;
-        (cast(Arc!T)this).__retain();
+    ref T opCast(T : typeof(this))() inout nothrow {
+        return *(cast(T*)(cast(void**)&rc.value));
     }
 }
 
-// Base RefCounted.
-private mixin template RcBase(T) {
+//
+//          IMPLEMENTATION DETAILS
+//
+private:
+
+struct __rc(T) {
 private:
 @nogc:
-    static struct __rc {
-    private:
-    @nogc:
-        uint refs;
-        T value;
-    }
-
-    __rc* rc;
-
-    pragma(inline, true)
-    void __retain() @trusted nothrow {
-        if (rc) {
-            nu_atomic_add_32(rc.refs, 1);
-        }
-    }
-
-    pragma(inline, true)
-    void __release() @trusted nothrow {
-        if (rc) {
-            if (nu_atomic_sub_32(rc.refs, 1) == 1) {
-                nogc_delete(rc.value);
-                nu_free(rc);
-            }
-        }
-        this.rc = null;
-    }
-
-public:
-    alias value this;
-
-    /**
-        The value stored in the rc type.
-    */
-    @property T value() @safe => rc ? rc.value : T.init;
+    uint refs;
+    T value;
 }
--- tests/ut/rc.d
+++ tests/ut/rc.d
@@ -4,18 +4,23 @@ import numem.lifetime : nogc_delete;
 
 @("Rc!int")
 unittest {
-    Rc!int a = 24;
-    assert(a == 24);
+    __gshared size_t rct_objects = 1;
+    static struct RCT1 {
+        int v;
+        alias v this;
+
+        ~this() @nogc { rct_objects--; }
+        this(ref return scope inout(typeof(this)) rhs) @nogc { rct_objects++; v = rhs.v; }
+        this(int value) @nogc {
+            rct_objects++;
+            v = value;
+        }
+    }
+
+    Rc!RCT1 a = RCT1(42);
+    assert(rct_objects == 1);
+    assert(a.v == 42);
     
     a.release();
-    assert(a == 0);
-}
-
-@("Arc!int")
-unittest {
-    Arc!int a = 24;
-    assert(a == 24);
-    
-    nogc_delete(a);
-    assert(a == 0);
+    assert(rct_objects == 0);
 }
